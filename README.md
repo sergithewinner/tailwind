e1824664582d154c2bd260da8f58043fb9a512a5
sergithewinner <sergithewinner@gmail.com>
2021-02-05 00:56:18 +0400

    ok

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# tailwind
+# TailwindCSS Cheat Sheet
---
+++ npm.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# npm init -y
+
+# npm install tailwindcss@latest postcss@latest autoprefixer@latest
+# npm run build:css
+
+# npx tailwind init
+
+# other commands
+# npm i tailwindcss
+# npm run build:css
+# npx tailwindcss-cli@latest build -o dist/style.css
+# npm install autoprefixer
+# npx tailwindcss init
+# npm run build:css
+# npm install tailwindcss@latest postcss@latest autoprefixer@latest
+
+
+#new way :
+# npm init -y
+# npm install tailwindcss@latest postcss@latest autoprefixer@latest
+# npm run build:css
+# npx tailwindcss init
+
+
+#old way:
+# npm init -y
+# npm i tailwindcss
+# npm i postcss
+# npm i autoprefixer
+# npx tailwind init
+
+#to run project
+# npm i
+
