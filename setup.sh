fa647b2e758566978e39e6c434d86f96ad5255de
sergithewinner <sergithewinner@gmail.com>
2021-02-05 18:50:35 +0400

    ok

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ cd project/dist/ && touch index.html style.css
 cd ../src/ && touch style.css
 cd ../ && npm init -y
 npm install tailwindcss@latest postcss@latest autoprefixer@latest && npx tailwindcss init
+# jq '.scripts.test = "tailwind build src/style.css -o dist/style.css"' package.json|sponge package.json
 # npm run build:css
 
 
