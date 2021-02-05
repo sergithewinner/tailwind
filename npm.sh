a653c1ed6f876c73e322c1553c3f4d37b17a7df8
sergithewinner <sergithewinner@gmail.com>
2021-02-05 14:21:11 +0400

    ok

--- npm.sh
+++ npm.sh
@@ -1,35 +1,16 @@
 #!/bin/bash
-# npm init -y
-
-# npm install tailwindcss@latest postcss@latest autoprefixer@latest
-# npm run build:css
-
-# npx tailwind init
-
-# other commands
-# npm i tailwindcss
-# npm run build:css
-# npx tailwindcss-cli@latest build -o dist/style.css
-# npm install autoprefixer
-# npx tailwindcss init
-# npm run build:css
-# npm install tailwindcss@latest postcss@latest autoprefixer@latest
+mkdir -p project/dist && mkdir project/src
+cd project/dist && touch index.html style.css
+cd ..
+cd src/ && touch style.css
+cd ..
+npm init -y
+npm install tailwindcss@latest postcss@latest autoprefixer@latest
+npx tailwindcss init
 
 
-#new way :
-# npm init -y
-# npm install tailwindcss@latest postcss@latest autoprefixer@latest
-# npm run build:css
-# npx tailwindcss init
 
 
-#old way:
-# npm init -y
-# npm i tailwindcss
-# npm i postcss
-# npm i autoprefixer
-# npx tailwind init
 
-#to run project
-# npm i
+
 
